895ccb3fb125acb031b4d09d668405fdcfba0469
mani <manibharathidct@gmail.com>
2022-09-11 16:56:06 +0530

    befor ui

--- src/app/components/viewstatement/viewstatement.component.ts
+++ src/app/components/viewstatement/viewstatement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shares/auth.service';
 import { ProfilService } from 'src/app/shares/profil.service';
 import { TokenStorageService } from 'src/app/shares/token-storage.service';
@@ -9,7 +9,7 @@ import { TokenStorageService } from 'src/app/shares/token-storage.service';
   styleUrls: ['./viewstatement.component.scss']
 })
 export class ViewstatementComponent implements OnInit {
-
+  @Input() count:any
   constructor(private profil:ProfilService ,private tokenStorage:TokenStorageService) { }
   items:any
   check(data:any):any{
@@ -23,11 +23,19 @@ export class ViewstatementComponent implements OnInit {
   default:string='No Transaction Found'
 
   ngOnInit(): void {
+    
     this.profil.viewstatement({token:this.tokenStorage.getToken()}).subscribe(res => {
       // alert(res.message)
+      console.log(this.count)
       if(res.status){
+        if(this.count!=undefined){
+          this.items=res.data
+          this.items.slice(0, 1)
+        }
+        else{
+          this.items=res.data
+        }
         
-        this.items=res.data
       }})
       
   }
